500bcb01e0cc38529f9aff5069c43ebeb6f5a3b6
MT 古谷 南旺 <nao.furutani@metateam.co.jp>
2026-05-11 17:07:10 +0900

    書籍管理アプリ内の書籍登録画面を

--- src/main/java/jp/co/metateam/library/controller/BookController.java
+++ src/main/java/jp/co/metateam/library/controller/BookController.java
@@ -25,30 +25,37 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class BookController {
     
-    private final BookMstService bookMstService;
+    private final BookMstService bookMstService;//controllerはDBを操作しないルール
 
-    @Autowired
+    @Autowired//springが自動でオブジェクトを渡す
     public BookController(BookMstService bookMstService){
         this.bookMstService = bookMstService;
     }
 
-    @GetMapping("/book/index")
+    @GetMapping("/book/index")//書籍画面を表示
     public String index(Model model) {
         // 書籍を全件取得
         List<BookMstDto> bookMstList = this.bookMstService.findAvailableWithStockCount();
         
-        model.addAttribute("bookMstList", bookMstList);
-
+        model.addAttribute("bookMstList", bookMstList);//HTMLへの橋渡し
         return "book/index";
     }
 
-    @GetMapping("/book/add")
+    @GetMapping("/book/add")//登録画面を表示
     public String add(Model model) {
-        if (!model.containsAttribute("bookMstDto")) {
-            model.addAttribute("bookMstDto", new BookMstDto());
+        if (!model.containsAttribute("bookMstDto")) {//モデルにbookMstDtoがない場合
+            model.addAttribute("bookMstDto", new BookMstDto()); //空の箱をつくってHTMLに渡す
         }
 
         return "book/add";
     }
-    
+    @PostMapping("/book/add")//登録処理
+    public String add(BookMstDto bookMstDto//画面の入力データを受け取る
+    ){
+        this.bookMstService.save(bookMstDto);//DBに受け取ったデータの登録処理お願いする
+        return "redirect:/book/index";//書籍一覧に戻る
+
+    }
+
+
 }
