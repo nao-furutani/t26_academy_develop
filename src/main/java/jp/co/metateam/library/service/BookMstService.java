f0f2e9aebd06c3dd7ec469449570a55877f4107d
nao-furutani <nao.furutani@metateam.co.jp>
2026-05-15 09:46:30 +0900

    BookMstService31差分修正

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -28,6 +28,7 @@ public class BookMstService {
     public List<BookMstDto> findAvailableWithStockCount() {
         List<BookMst> books = this.bookMstRepository.findLimitedBook();
         List<BookMstDto> bookMstDtoList = new ArrayList<BookMstDto>();
+        
         // 書籍の在庫数を取得
         // FIXME: 現状は書籍ID毎にDBに問い合わせている。一度のSQLで完了させたい。
         for (int i = 0; i < books.size(); i++) {
